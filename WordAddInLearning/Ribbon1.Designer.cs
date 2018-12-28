5bac1dad32631406a635e1ef9a5bfbc9514b4ab8
Xiaowen Cao <xw2015@outlook.com>
2018-12-28 21:40:52 +0800

    第一次更改

---
+++ WordAddInLearning/Ribbon1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace WordAddInLearning
+{
+    partial class Ribbon1 : Microsoft.Office.Tools.Ribbon.RibbonBase
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        public Ribbon1()
+            : base(Globals.Factory.GetRibbonFactory())
+        {
+            InitializeComponent();
+        }
+
+        /// <summary> 
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region 组件设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabLearning = this.Factory.CreateRibbonTab();
+            this.group1 = this.Factory.CreateRibbonGroup();
+            this.button1 = this.Factory.CreateRibbonButton();
+            this.editBox1 = this.Factory.CreateRibbonEditBox();
+            this.tabLearning.SuspendLayout();
+            this.group1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabLearning
+            // 
+            this.tabLearning.Groups.Add(this.group1);
+            this.tabLearning.Label = "学习测试";
+            this.tabLearning.Name = "tabLearning";
+            // 
+            // group1
+            // 
+            this.group1.Items.Add(this.editBox1);
+            this.group1.Items.Add(this.button1);
+            this.group1.Label = "group1";
+            this.group1.Name = "group1";
+            // 
+            // button1
+            // 
+            this.button1.Label = "button1";
+            this.button1.Name = "button1";
+            this.button1.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.button1_Click);
+            // 
+            // editBox1
+            // 
+            this.editBox1.Label = "editBox1";
+            this.editBox1.Name = "editBox1";
+            // 
+            // Ribbon1
+            // 
+            this.Name = "Ribbon1";
+            this.RibbonType = "Microsoft.Word.Document";
+            this.Tabs.Add(this.tabLearning);
+            this.Load += new Microsoft.Office.Tools.Ribbon.RibbonUIEventHandler(this.Ribbon1_Load);
+            this.tabLearning.ResumeLayout(false);
+            this.tabLearning.PerformLayout();
+            this.group1.ResumeLayout(false);
+            this.group1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        internal Microsoft.Office.Tools.Ribbon.RibbonTab tabLearning;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup group1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton button1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonEditBox editBox1;
+    }
+
+    partial class ThisRibbonCollection
+    {
+        internal Ribbon1 Ribbon1
+        {
+            get { return this.GetRibbon<Ribbon1>(); }
+        }
+    }
+}
---
+++ WordAddInLearning/Ribbon1.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Office.Tools.Ribbon;
+using Word = Microsoft.Office.Interop.Word;
+using System.Windows.Forms;
+
+
+namespace WordAddInLearning
+{
+    public partial class Ribbon1
+    {
+        private void Ribbon1_Load(object sender, RibbonUIEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, RibbonControlEventArgs e)
+        {
+            var doc = Globals.ThisAddIn.Application.ActiveDocument;
+
+            switch (editBox1.Text)
+            {
+                case "1":
+                    //选中0-7之间的7个字符
+                    doc.Range(0, 7).Select();
+                    break;
+                case "2":
+                    //选中所有字符
+                    doc.Content.Select();
+                    break;
+                case "3":
+                    //选中第二个句子
+                    doc.Sentences[1].Select();
+                    break;
+                case "4":
+                    //选中第一和第二个句子
+                    var sentences = doc.Sentences;
+                    if (sentences.Count >= 2)
+                    {
+                        doc.Range(sentences[1].Start,sentences[2].End).Select();
+                    }
+                    break;
+                case "5":
+                    //获取第二个句子的起止范围
+                    var rang = doc.Sentences[2];
+                    System.Windows.Forms.MessageBox.Show($"{rang.Start}+{rang.End}");
+                    break;
+                case "6":
+                    ReplaceParagraphText();
+                    break;
+                case "7":
+                    ExpandRange();
+                    break;
+                case "8":
+                    ResetRange();
+                    break;
+                case "9":
+                    CollapseRange();
+                    break;
+                case "10":
+                    SelectionInsertText();
+                    break;
+                case "11":
+                    RangeFormat();
+                    break;
+                case "12":
+                    GetAllStyles();
+                    break;
+                case "13":
+                    CreateCustomerStyle();
+                    break;
+                case "14":
+                    CreateBookMark();
+                    break;
+                default:
+                    break;
+            }
+            
+
+            
+            
+        }
+
+
+        private void ReplaceParagraphText()
+        {
+            var doc = Globals.ThisAddIn.Application.ActiveDocument;
+            var firstRange = doc.Paragraphs[1].Range;
+            var secondRange = doc.Paragraphs[2].Range;
+
+            var firstString = firstRange.Text;
+            var secondString = secondRange.Text;
+
+            firstRange.Select();
+            MessageBox.Show(firstRange.Text);
+            secondRange.Select();
+            MessageBox.Show(secondRange.Text);
+
+            object charUnit = Word.WdUnits.wdCharacter;
+            object move = -1;
+
+            firstRange.MoveEnd(ref charUnit, ref move);
+
+            firstRange.Text = $"第一段新内容——{DateTime.Now.ToString()}";
+            secondRange.Text = $"第二段新内容——{DateTime.Now.ToString()}";
+
+            firstRange.Select();
+            MessageBox.Show(firstRange.Text);
+            secondRange.Select();
+            MessageBox.Show(secondRange.Text);
+
+            move = 1;
+            firstRange.MoveEnd(ref charUnit, ref move);
+            secondRange.Delete();
+            firstRange.Text = firstString;
+            firstRange.InsertAfter(secondString);
+            firstRange.Select();
+
+
+        }
+
+        private void ExpandRange()
+        {
+            var range = Globals.ThisAddIn.Application.ActiveDocument.Range(0, 7);
+            //起始位置向右移7个字符
+            range.MoveStart(Word.WdUnits.wdCharacter, 7);
+            //终止位置向右移7个字符
+            range.MoveEnd(Word.WdUnits.wdCharacter, 7);
+            range.Select();
+        }
+
+        private void ResetRange()
+        {
+            var range = Globals.ThisAddIn.Application.ActiveDocument.Range(0, 7);
+            range.SetRange(Globals.ThisAddIn.Application.ActiveDocument.Sentences[2].Start,
+                Globals.ThisAddIn.Application.ActiveDocument.Sentences[5].End);
+            range.Select();
+        }
+
+        private void CollapseRange()
+        {
+            var range = Globals.ThisAddIn.Application.ActiveDocument.Paragraphs[1].Range;
+            range.Collapse(Word.WdCollapseDirection.wdCollapseStart);
+            range.Text = "段前新文本";
+            range.Collapse(Word.WdCollapseDirection.wdCollapseEnd);
+            range.Text = "段后新文本";
+            range.Select();
+        }
+
+        private void SelectionInsertText()
+        {
+            var currentSelection = Globals.ThisAddIn.Application.Selection;
+            var userOvertype = Globals.ThisAddIn.Application.Options.Overtype;
+            if (Globals.ThisAddIn.Application.Options.Overtype)
+            {
+                Globals.ThisAddIn.Application.Options.Overtype = false;
+            }
+            if (currentSelection.Type == Word.WdSelectionType.wdSelectionIP)
+            {
+                currentSelection.TypeText("《插入的新文本》");
+                currentSelection.TypeParagraph();//插入新行
+            }
+            else if (currentSelection.Type == Word.WdSelectionType.wdSelectionNormal)
+            {
+                if (Globals.ThisAddIn.Application.Options.ReplaceSelection)
+                {
+                    object direction = Word.WdCollapseDirection.wdCollapseStart;
+                    currentSelection.Collapse(ref direction);
+                }
+                currentSelection.TypeText("《插入的新文本》");
+                currentSelection.TypeParagraph();
+            }
+            else
+            {
+
+            }
+            //还原用户初始设置
+            Globals.ThisAddIn.Application.Options.Overtype = userOvertype;
+        }
+
+        private void RangeFormat()
+        {
+            var doc = Globals.ThisAddIn.Application.ActiveDocument;
+            var range = doc.Paragraphs[1].Range;
+
+            range.Font.Size = 14;
+            range.Font.Name = "宋体";
+            range.ParagraphFormat.Alignment = Word.WdParagraphAlignment.wdAlignParagraphLeft;
+
+            range.Select();
+            MessageBox.Show("格式化第一段");
+
+            object numTimes = 3;
+            doc.Undo(ref numTimes);
+            range.Select();
+            MessageBox.Show("撤销 3 步操作");
+
+            object indentStyle = "引用";
+            range.set_Style(ref indentStyle);
+
+            range.Select();
+            MessageBox.Show("应用样式");
+
+            object numTimes1 = 1;
+            doc.Undo(ref numTimes1);
+
+            range.Select();
+            MessageBox.Show("撤销 1 步");
+        }
+
+        private void GetAllStyles()
+        {
+            //var styleString = Globals.ThisAddIn.Application.ActiveDocument.Paragraphs[1].Range.get_Style();
+            //MessageBox.Show(styleString.NameLocal);
+
+            var selection = Globals.ThisAddIn.Application.Selection;
+            foreach (Word.Style style in Globals.ThisAddIn.Application.ActiveDocument.Styles)
+            {
+                if (style.Type == Word.WdStyleType.wdStyleTypeParagraph)
+                {
+                    selection.TypeText(style.NameLocal + "\t");
+                }
+            }
+        }
+
+        private void CreateCustomerStyle()
+        {
+            var doc = Globals.ThisAddIn.Application.ActiveDocument;
+            doc.Styles.Add("我的样式", Word.WdStyleType.wdStyleTypeParagraph);
+            Word.Style myStyle = doc.Styles["我的样式"];
+            myStyle.AutomaticallyUpdate = true;
+            myStyle.Font.Name = "楷体";
+            myStyle.Font.Size = 22;
+            myStyle.ParagraphFormat.Alignment = Word.WdParagraphAlignment.wdAlignParagraphCenter;
+        }
+
+        private void CreateBookMark()
+        {
+            Globals.ThisAddIn.Application.ActiveDocument.Range(0, 7).Bookmarks.Add("测试书签");
+            
+        }
+    }
+}
